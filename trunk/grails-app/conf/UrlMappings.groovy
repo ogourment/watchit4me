f1b06b7d0afbd151f1840a717c53636e70db1d2f
ogourment <ogourment@c1f5eb84-74a6-11de-86bc-2d44e85db88f>
2009-07-21 03:06:13 +0000

    with google user detection, but no sign out link

--- trunk/grails-app/conf/UrlMappings.groovy
+++ trunk/grails-app/conf/UrlMappings.groovy
@@ -5,7 +5,7 @@ class UrlMappings {
 			 // apply constraints here
 		  }
 	  }
-      "/"(view:"/index")
+      "/" { controller = "home" }
 	  "500"(view:'/error')
 	}
 }
---
+++ trunk/grails-app/controllers/watchit4me/HomeController.groovy
@@ -0,0 +1,22 @@
+
+package watchit4me
+
+import com.google.appengine.api.users.UserServiceFactory
+
+class HomeController {
+	
+	def index = {
+		
+		def userService = UserServiceFactory.getUserService()
+		
+        String thisURL = request.requestURI
+        if (request.userPrincipal) {
+			def signout = userService.createLogoutURL(thisURL)
+            flash.message = """<p>Hello, ${request.userPrincipal.name}!
+			You can <a href=\"${signout}"\">sign out</a>.</p>"""			
+        } else {
+			def signin = userService.createLoginURL(thisURL)
+            render "<p>Please <a href=\"${signin}\">sign in</a>.</p>"
+        }
+	}
+}
